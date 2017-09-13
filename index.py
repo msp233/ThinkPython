1eb1a085086d736c211f7ed7c314c1ae043994ea
v_shipmao(茆世平) <v_shipmao@tencent.com>
2017-09-13 21:44:56 +0800

    0-32page total:172

--- index.py
+++ index.py
@@ -1,2 +1,34 @@
 print(1+1)
 print("hello world")
+print(4)
+a = type('hello world')
+b = type(123)
+c = type(123.567)
+d = type("123")
+e = type('123')
+f = type("a")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+#print(hhh)
+hhh = 'abc'
+print(hhh)
+print(type(hhh))
+aaa = hhh+'3';
+print(aaa);
+print(type(aaa));
+hhh = 34
+print(hhh)
+print(type(hhh))
+
+print(3**3);
+#    ** 幂  3的3次幂
+minute = 59
+asdf = minute/60
+print(asdf)
+aas = 'abc';
+asa = aas*3;
+print(asa);
